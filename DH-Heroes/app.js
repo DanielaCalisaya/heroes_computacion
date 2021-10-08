49093d59f4259c30e43fc104d5190389b48f3bbc
DanielaCalisaya <hdaniela127c@gmail.com>
2021-10-08 11:49:50 -0500

    arregle problema de rutas

--- DH-Heroes/app.js
+++ DH-Heroes/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const PORT = 3030;
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 
 app.get('/', (req, res) => {
